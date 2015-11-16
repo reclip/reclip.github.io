50a7a8dc69f0852f93d3acf348cd742fa5add052
Victor <theonlypwner@users.noreply.github.com>
2015-11-15 17:05:11 -0700

    Initial commit

---
+++ assets/re/global.js
@@ -0,0 +1,25 @@
+$(function(){
+	// timeago
+	$.timeago.settings.refreshMillis = 5000;
+	$.timeago.settings.strings = {
+		prefixAgo: null,
+		prefixFromNow: null,
+		suffixAgo: "ago",
+		suffixFromNow: "from now",
+		seconds: "%d seconds",
+		minute: "a minute",
+		minutes: "%d minutes",
+		hour: "an hour",
+		hours: "%d hours",
+		day: "a day",
+		days: "%d days",
+		month: "a month",
+		months: "%d months",
+		year: "a year",
+		years: "%d years",
+		wordSeparator: " ",
+		numbers: []
+	};
+	// Invoke timeago
+	$(".timeago").timeago();
+});
